492b72bfa153198c6c8734cb70d2448f7d43d0a7
yesmynamedongwoo <kdw3917ss@gmail.com>
2022-03-26 01:58:59 +0900

    알고리즘 22.03.26 달팽이 백준 2869 풀었음

---
+++ algorithm/backjoon_10869.py
@@ -0,0 +1,10 @@
+# 7 3
+A, B = map(int, input().split())
+# A = int(A)
+# B = int(B)
+
+print(A + B)
+print(A - B)
+print(A * B)
+print(A // B)
+print(A % B)
---
+++ algorithm/backjoon_2588.py
@@ -0,0 +1,15 @@
+a = int(input())   #472
+b = (input())  #385
+
+aa = list(b)
+
+c = int(aa[0]) #3
+d = int(aa[1]) #8
+e = int(aa[2]) #5
+
+bb = int(b) # 정수형으로 치환
+
+print(a * e)
+print(a * d)
+print(a * c)
+print(a * bb)
