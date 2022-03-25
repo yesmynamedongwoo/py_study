73a6eedb3f661cace35a090c7b030d9cec3176a4
yesmynamedongwoo <kdw3917ss@gmail.com>
2022-03-26 02:53:02 +0900

    구름 43092

---
+++ algorithm/groom_43092.py
@@ -0,0 +1,22 @@
+a = int(input())
+
+def match():
+	if a > 1:
+		sum = 0
+		for i in range(1, a):
+			sum += i
+		return print(sum)
+	else:
+		return
+
+match()
+
+
+
+# n = int(input())
+# count = 0
+
+# for a in range(1,n):
+# 	count = count + a
+
+# print(count)
